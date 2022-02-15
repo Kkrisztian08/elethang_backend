b67a17f965a4ccbba4f2529c0ebf4f5082caa0c4
Kkrisztian08 <k.krisztian0830@gmail.com>
2022-02-15 12:15:56 +0100

    Virtual Adoption törlése és ProgramHD osztályok

--- app/Http/Requests/ProgramHourAndDay/ProgramHourAndDayCreate.php
+++ app/Http/Requests/ProgramHourAndDay/ProgramHourAndDayCreate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\ProgramHourAndDay;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -13,7 +13,7 @@ class ProgramHourAndDayCreate extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
