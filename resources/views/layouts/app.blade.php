992c85eba901e11099a050f267bfbe45d56cba21
Zita <ambrus.zita99@gmail.com>
2022-03-26 16:39:15 +0100

    Előzőhöz

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,14 +111,15 @@
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-end" style="text-align: center;" aria-labelledby="navbarDropdown">
                                     
-                                    <a class="dropdown-item" href="{{ url('/profile') }}">
-                                        Profil szerkesztése
-                                    </a>
                                     <a class="dropdown-item" href="{{ url('/pictures') }}">
                                         Képek
                                     </a>
+                                    <a class="dropdown-item" href="{{ url('/profile') }}">
+                                        Profil szerkesztése
+                                    </a>
+                                    <hr class="dropdown-divider">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
