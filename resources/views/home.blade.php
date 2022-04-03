41e3836839baa00d6126d32bfbce1c4c366f4e52
Zita <ambrus.zita99@gmail.com>
2022-04-03 18:47:58 +0200

    home.blade.php magyarosítása

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    <h1>Üdvözölünk</h1>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                            </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <h2>A bejelentkezés sikeresen megtörtént</h2>
                 </div>
             </div>
         </div>
