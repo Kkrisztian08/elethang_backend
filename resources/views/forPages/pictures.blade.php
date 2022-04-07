fe7dd8942a861a9f226b800adf3dff8185e93ba1
Zita <ambrus.zita99@gmail.com>
2022-04-08 00:02:12 +0200

    Képek oldal kinézet tisztázása

--- resources/views/forPages/pictures.blade.php
+++ resources/views/forPages/pictures.blade.php
@@ -2,71 +2,73 @@
 
 @section('content')
 
-    <div class="container">
-        <h2>Kép feltoltese</h2>
-        <img type="button" class="img-fluid float-end" src="/img/feltolt.png" alt="Mentes">
-        <br><br>
-        
+<div class="container">
+    <h2 style="text-align: center;">Új kép feltöltése</h2>
 
+    <div class="container" style="padding: 5em;">
+        <form method="post" action="{{ route('images.store') }}" enctype="multipart/form-data">
+            @csrf
+            <div class="image">
+                <label>
+                    <h4>Kép választása</h4>
+                </label>
+                <input type="file" class="form-control" required name="image">
+            </div>
+            <div class="description">
+                <label>
+                    <h4>Leírás hozzáadása</h4>
+                </label>
+                <input type="text" class="form-control" required name="description">
+            </div>
 
-        <div class="container">
-            <form method="post" action="{{ route('images.store') }}" 
-                    enctype="multipart/form-data">
-                @csrf
-                <div class="image">
-                    <label><h4>Kép hozzáadása</h4></label>
-                    <input type="file" class="form-control" required name="image">
-                </div>
-                <div class="description">
-                    <label><h4>Leírás hozzáadása</h4></label>
-                    <input type="text" class="form-control" required name="description">
-                </div>
+            <a class="post_button">
+                <img src="/img/feltolt.png" type="submit" class="img-fluid float-end" alt="torol">
+            </a>
+        </form>
 
-                <div class="post_button">
-                <button type="submit" class="btn btn-success">Add</button>
-                </div>
-            </form>
-            
-            @if ($message = Session::get('success'))
+        @if ($message = Session::get('success'))
 
-                <div class="alert alert-success alert-dismissible">
-                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                    <strong>{{ $message }}!</strong>
-                </div>
-            @endif
+        <div class="alert alert-success alert-dismissible">
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            <strong>{{ $message }}!</strong>
         </div>
-
-
-
-        <div class="container">
-        <h3>Képeim</h3><hr>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Kép</th>
-                    <th scope="col">Leírása</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($imageData as $data)
-                    <tr>
-                        <td>
-                            <img src="{{ url('public/Image/'.$data->image) }}" style="height: 100px; width: 150px;">
-                        </td>
-                        <td>{{$data->description}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        @endif
     </div>
 
 
 
 
 
+    <h2 style="text-align: center;">Képeim</h2>
+    <div class="container">
+        <div class="owl-carousel owl-theme mt-5">
+            @foreach($imageData as $data)
+            <div class="item">
+                <div class="card szivKartyB">
+                    <img style="padding-top: 1.9em;" src="{{ url('public/Image/'.$data->image) }}" class="card-img-top img-fluid" alt="kepek" />
 
-
-        
+                    <div class="card-body">
+                        <p class="d-inline-block text-truncate" style="max-width: 150px;">
+                            {{$data->description}}
+                        </p>
+                    </div>
+                    <a class="szivB" style="text-align: right; padding-right: 0.5em; " href="#">
+                        <img src="/img/kuka.png" type="button" class="img-fluid float-end" alt="torol">
+                    </a>
+                </div>
+            </div>
+            @endforeach
+        </div>
     </div>
+</div>
+
+
+
+
+
+
+
+
+</div>
 
 @endsection
